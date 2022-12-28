42d79958c13c58c242c8e6dfe484ef53376e6a38
CosmicDust19 <smh01.2019@gmail.com>
2022-12-28 22:52:49 +0300

    deployment

--- src/main/java/edu/estu/estufastingrestapi/core/api/common/Origin.java
+++ src/main/java/edu/estu/estufastingrestapi/core/api/common/Origin.java
@@ -6,4 +6,6 @@ import lombok.experimental.UtilityClass;
 public class Origin {
     public final String LOCALHOST_3000 = "http://localhost:3000";
     public final String LOCALHOST_3001 = "http://localhost:3001";
+    public final String ESTU_FASTING_UI = "https://estufasting.netlify.app/";
+    public final String ESTU_FASTING_MS_UI = "https://estufasting-ms.netlify.app/";
 }
